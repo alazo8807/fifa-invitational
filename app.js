726638f8456c3ae01e493fbed02c72c7a3d05c08
Alejandro Lazo Hernandez <alazo@Alejandros-MacBook-Pro.local>
2020-09-08 16:31:07 -0400

    add joi to validate post request

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -25,6 +26,13 @@ app.get('/api/tournaments/:id', (req, res) => {
 });
 
 app.post('/api/tournaments', (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+
+  const result = schema.validate(req.body);
+  if (result.error) return res.status(400).send(result.error.details[0].message);
+
   const tournament = {
     id: tournaments.length + 1,
     name: req.body.name
