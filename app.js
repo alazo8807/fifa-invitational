6c26af0bc6a9c41a08e6c53475026ff44848f921
Alejandro Lazo Hernandez <alazo@Alejandros-MacBook-Pro.local>
2020-09-08 13:04:56 -0400

    add initial get routes

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
 
+const tournaments = [
+  { id: 1, name: 'tournament 1' },
+  { id: 2, name: 'tournament 3' },
+  { id: 3, name: 'tournament 3' },
+];
+
 app.get('/', (req, res) => {
   res.send('Hello');
+});
+
+app.get('/api/tournaments', (req, res) => {
+  res.send(tournaments);
 })
 
-app.listen(3000, () => console.log('Listening on port 3000...'));
+app.get('/api/tournaments/:id', (req, res) => {
+  const tournament = tournaments.find(t => t.id === parseInt(req.params.id));
+  if (!tournament) res.status(404).send('Tournament with provided ID does not exist');
+  res.send(tournament);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`Listening on port ${port}...`));
