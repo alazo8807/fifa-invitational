8663a612707977d45c6ef498a766179029f26ef2
Alejo <alazo8807@gmail.com>
2020-11-07 15:11:35 -0500

    Add authentication logic (#4)

--- startup/routes.js
+++ startup/routes.js
@@ -1,5 +1,7 @@
 const matches = require('../routes/matches');
 const tournaments = require('../routes/tournaments');
+const users = require('../routes/users');
+const auth = require('../routes/auth');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require("cors");
@@ -11,4 +13,6 @@ module.exports = function(app) {
   app.use(cors());
   app.use('/api/matches', matches);
   app.use('/api/tournaments', tournaments);
+  app.use('/api/users', users);
+  app.use('/api/auth', auth);
 }
