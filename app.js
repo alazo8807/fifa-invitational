24a706e9c8d4b528ae24653b224042b7b20365b6
Alejandro Lazo Hernandez <alazo8807@gmail.com>
2020-12-04 15:13:20 -0500

    update port to listen

--- app.js
+++ app.js
@@ -19,4 +19,4 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(3000, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
