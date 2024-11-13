3ecd0ca1a7a38bb456f2af392d09b253bf7e87d0
alazo8807 <alazo8807@gmail.com>
2024-11-13 07:46:47 -0500

    revert hardcoded string

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ if (!conf.get("jwtPrivateKey")) {
   throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
 }
 
-const db_uri = "mongodb+srv://admin:i2pm7BY34d22oE6I@cluster0.h0csx.mongodb.net/fifa-invitational?retryWrites=true&w=majority&appName=Cluster0"
-// mongoose.connect(conf.get("db"))
+mongoose.connect(conf.get("db"))
 mongoose.connect(db_uri)
   .then(() => console.log("Connected to MongoDb"))
   .catch(() => console.log("Error connecting to MongoDb"))
